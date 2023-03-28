dc7342ac7d6e20338d86b6a6e8191f3c4e4a034c
MirazamTUT <mmiromonov111@gmail.com>
2023-03-28 14:44:14 +0500

    Project ochildi va Model lar qo'shildi

---
+++ TestSchool/Model/Address.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestSchool.Model
+{
+    public class Address
+    {
+        [Key]
+        public int AddressID { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public int HomeNumber { get; set; }
+
+        public List<Student> student { get; set; }
+    }
+}
---
+++ TestSchool/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestSchool.Model
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        public string StudentName { get; set; }
+
+        public string StudentSureName { get; set; }
+
+        public int StudentAddressID { get; set; }
+    }
+}
