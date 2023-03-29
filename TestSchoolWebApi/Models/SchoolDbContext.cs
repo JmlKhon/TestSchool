23c668c01e1425e161c5d2d4b49add0b88ba3c7b
unknown <isroilov1922@gmail.com>
2023-03-29 12:06:09 +0500

    Add db connection completed

---
+++ TestSchoolWebApi/Models/SchoolDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TestSchool.Models
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) :
+             base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Address> Address { get; set; }
+    }
+}
