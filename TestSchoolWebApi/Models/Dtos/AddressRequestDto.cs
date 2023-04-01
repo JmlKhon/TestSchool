59fe7bfb8260b5e9e906e6bb77df0ecb7b3ebb5a
misterIminjonov <gofurjon128@gmail.com>
2023-04-01 16:18:18 +0500

    Xatolar to'g'rilandi

---
+++ TestSchoolWebApi/Models/Dtos/AddressRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace TestSchool.Models.Dtos
+{
+    public class AddressRequestDto
+    {
+        public string AddressId { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Region { get; set; }
+
+        public string Street { get; set; }
+    }
+}
---
+++ TestSchoolWebApi/Models/Dtos/AddressResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace TestSchool.Models.Dtos
+{
+    public class AddressResponseDto
+    {
+        public string Addressid { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+    }
+}
