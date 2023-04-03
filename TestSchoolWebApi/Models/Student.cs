046dca9df6e13327ece8a91b6f00d14066d3225f
MirazamTUT <mmiromonov111@gmail.com>
2023-04-03 15:03:16 +0500

    Addition to Student model

--- TestSchoolWebApi/Models/Student.cs
+++ TestSchoolWebApi/Models/Student.cs
@@ -8,6 +8,10 @@
 
         public string LastName { get; set; }
 
+        public int Phone { get; set; }
+
+        public string Email { get; set; }
+
         public int AddressId { get; set; }
 
         public Address Address { get; set; }
