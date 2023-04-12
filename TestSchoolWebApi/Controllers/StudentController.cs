8f8c2fdaa45884b12dff35d52a6eb3a9ff92df77
sher_violino1989@list.ru <sher_violino1989@list.ru>
2023-04-12 23:41:37 +0500

    Exception ozgartirildi

--- TestSchoolWebApi/Controllers/StudentController.cs
+++ TestSchoolWebApi/Controllers/StudentController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TestSchool.Models;
 using TestSchool.Models.Dtos;
 using TestSchool.Repository;
@@ -20,74 +19,114 @@ namespace TestSchool.Controllers
         [HttpPost]
         public ActionResult<int> AddStudent(StudentRequestDto studentRequestDto)
         {
-            var student = new Student
+            try
             {
-                FirstName = studentRequestDto.FirstName,
-                LastName = studentRequestDto.LastName,
-                Email = studentRequestDto.Email,
-                Phone = studentRequestDto.Phone,
-                AddressId = studentRequestDto.AdressId,
-            };
-            _studentRepository.AddStudent(student);
-            return student.StudentId;
+                var student = new Student
+                {
+                    FirstName = studentRequestDto.FirstName,
+                    LastName = studentRequestDto.LastName,
+                    Email = studentRequestDto.Email,
+                    Phone = studentRequestDto.Phone,
+                    AddressId = studentRequestDto.AdressId,
+                };
+                _studentRepository.AddStudent(student);
+                return student.StudentId;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpGet("id")]
         public ActionResult<StudentResponseDto> GetStudent(int id)
         {
-            var student = _studentRepository.GetStudent(id);
-            var studentResponseDto = new StudentResponseDto
+            try
             {
-                StudentId = student.StudentId,
-                StudentName = ($"{student.FirstName} {student.LastName}"),
-                Address = ($"{student.Address.Country} {student.Address.City}"),
-            };
-            return studentResponseDto;
+                var student = _studentRepository.GetStudent(id);
+                var studentResponseDto = new StudentResponseDto
+                {
+                    StudentId = student.StudentId,
+                    StudentName = ($"{student.FirstName} {student.LastName}"),
+                    Address = ($"{student.Address.Country} {student.Address.City}"),
+                };
+                return studentResponseDto;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpGet]
         public ActionResult<List<StudentResponseDto>> GetStudents()
         {
-            var allStudents = _studentRepository.GetStudents();
-            var allStudentsResponseDto = new List<StudentResponseDto>();
-
-            for (int i = 0; i <= allStudents.Count() - 1; i++)
+            try
             {
-                allStudentsResponseDto.Add(new StudentResponseDto
+                var allStudents = _studentRepository.GetStudents();
+                var allStudentsResponseDto = new List<StudentResponseDto>();
+
+                for (int i = 0; i <= allStudents.Count() - 1; i++)
                 {
-                    StudentId = allStudents[i].StudentId,
-                    StudentName = ($"{allStudents[i].FirstName} {allStudents[i].LastName}"),
-                    Address = ($"{allStudents[i].Address.Country} {allStudents[i].Address.City}")
-                });
+                    allStudentsResponseDto.Add(new StudentResponseDto
+                    {
+                        StudentId = allStudents[i].StudentId,
+                        StudentName = ($"{allStudents[i].FirstName} {allStudents[i].LastName}"),
+                        Address = ($"{allStudents[i].Address.Country} {allStudents[i].Address.City}")
+                    });
+                }
+                return allStudentsResponseDto;
             }
-            return allStudentsResponseDto;
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpPut]
-        public ActionResult<int> UpdateStudent(StudentRequestDto studentRequestDto,int id)
+        public ActionResult<int> UpdateStudent(StudentRequestDto studentRequestDto, int id)
         {
-            var student = new Student
+            try
             {
-                FirstName = studentRequestDto.FirstName,
-                LastName = studentRequestDto.LastName,
-                Email = studentRequestDto.Email,
-                Phone = studentRequestDto.Phone,
-                AddressId = studentRequestDto.AdressId,
-            };
-            if (_studentRepository.GetStudent(id) != null)
-            {
-                student.StudentId = id;
-                _studentRepository.UpdateStudent(student);
+                var student = new Student
+                {
+                    FirstName = studentRequestDto.FirstName,
+                    LastName = studentRequestDto.LastName,
+                    Email = studentRequestDto.Email,
+                    Phone = studentRequestDto.Phone,
+                    AddressId = studentRequestDto.AdressId,
+                };
+                if (_studentRepository.GetStudent(id) != null)
+                {
+                    student.StudentId = id;
+                    _studentRepository.UpdateStudent(student);
+                }
+                return id;
             }
-            return id;
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
 
         [HttpDelete("id")]
         public ActionResult<int> DeleteStudent(int id)
         {
-            var student = _studentRepository.GetStudent(id);
-            _studentRepository.DeleteStudent(student);
-            return student.StudentId;
+            try
+            {
+                var student = _studentRepository.GetStudent(id);
+                _studentRepository.DeleteStudent(student);
+                return student.StudentId;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return StatusCode(StatusCodes.Status204NoContent);
+            }
         }
     }
 }
